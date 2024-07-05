baa04af23a9dc4d873b7f0bd912ac0ae7a1087c2
alexis asolarte <aasolarte@gmail.com>
2024-07-04 20:47:30 -0500

    actualizacion 3

--- 01-print.py
+++ 01-print.py
@@ -1,5 +1,8 @@
-print ("Hola mundo")
-print ("me encanta estudiar Python")
-print (300+255)
-print ("estudiar con \"python total\"\n es super")
-print ('Alexis'+' '+'Solarte')
+#print ("Hola mundo")
+#print ("me encanta estudiar Python")
+#print (300+255)
+#print ("estudiar con \"python total\"\n es super")
+#print ('Alexis'+' '+'Solarte')
+print (' linea 1\n linea 2\n linea 3')
+print ('a \t b\t c\t \n'
+       'd \t e\t f\t \n')
